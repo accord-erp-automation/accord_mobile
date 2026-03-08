8eb278aac05f7f938f36ea5e98182dba6419b65c
WIKKI <uzkingshark@gmail.com>
2026-03-08 13:48:10 +0500

    Move supplier dock slightly lower

--- lib/src/core/widgets/common_widgets.dart
+++ lib/src/core/widgets/common_widgets.dart
@@ -152,7 +152,7 @@ class ActionDock extends StatelessWidget {
     ];
 
     return Padding(
-      padding: const EdgeInsets.only(top: 26, bottom: 0),
+      padding: const EdgeInsets.only(top: 34, bottom: 0),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
